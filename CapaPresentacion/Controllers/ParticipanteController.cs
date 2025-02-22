579a2ba8a45b9864015800fabc67a2b7eefeadf7
IsaacEmerson-05 <72278816@ijamvirtual.edu.pe>
2025-02-21 20:36:07 -0500

    Isaac Avance - 4

---
+++ CapaPresentacion/Controllers/ParticipanteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaAplicacion;
+using CapaEntidades;
+
+namespace CapaPresentacion.Controllers
+{
+    public class ParticipanteController : Controller
+    {
+        // GET: Participante
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Participante/Create
+        [HttpPost]
+        public ActionResult Create(Participante participante)
+        {
+            if (ModelState.IsValid)
+            {
+                appParticipante.Instancia.InsertarParticipante(participante);
+                return RedirectToAction("Index");
+            }
+            return View(participante);
+        }
+    }
+}
